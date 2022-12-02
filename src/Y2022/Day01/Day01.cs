1d86bb22c49ef15e5122f6831d704d7ada0854c7
Jason Woods <jwoods@appliedsystems.com>
2022-12-01 22:59:00 -0600

    refactor: update to use Split instead of Segment

--- src/Y2022/Day01/Day01.cs
+++ src/Y2022/Day01/Day01.cs
@@ -1,13 +1,15 @@
-﻿using static MoreLinq.Extensions.SegmentExtension;
+﻿using static MoreLinq.Extensions.SplitExtension;
 
 namespace AoC.Y2022.Day01;
 
 public class Day01 : IAoCRunner<IReadOnlyList<IReadOnlyList<int>>, int>
 {
     public IReadOnlyList<IReadOnlyList<int>> ParseInput(string[] puzzleInput) => puzzleInput
-        .Select(l => int.TryParse(l, out var v) ? v : -1)
-        .Segment(n => n == -1)
-        .Select(ns => (IReadOnlyList<int>)ns.SkipWhile(n => n == -1).ToList())
+        .Split(
+            l => l == string.Empty,
+            ls => (IReadOnlyList<int>)ls
+                .Select(int.Parse)
+                .ToList())
         .ToList();
 
     public int RunPart1(IReadOnlyList<IReadOnlyList<int>> input) => input
