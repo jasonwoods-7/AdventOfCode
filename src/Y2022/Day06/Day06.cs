65040fa365ac332767954fbdc359b26e00947666
Jason Woods <jwoods@appliedsystems.com>
2022-12-01 21:56:42 -0600

    chore: adding AoC skeleton files

---
+++ src/Y2022/Day06/Day06.cs
@@ -0,0 +1,10 @@
+namespace AoC.Y2022.Day06;
+
+public class Day06 : IAoCRunner<string, int>
+{
+    public string ParseInput(string[] puzzleInput) => throw new NotImplementedException();
+
+    public int RunPart1(string input) => throw new NotImplementedException();
+
+    public int RunPart2(string input) => throw new NotImplementedException();
+}
---
+++ test/unit/Y2022/Day10/Day10Tests.cs
@@ -0,0 +1,66 @@
+﻿namespace AoC.Tests.Y2022.Day10;
+
+public class Day10Tests
+{
+    [Fact]
+    public void Example_Part1()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(InputHelpers.ReadInputFile("example.txt"));
+
+        // Act
+        var actual = runner.RunPart1(input);
+
+        // Assert
+        actual.Should().Be(0);
+    }
+
+    [Fact]
+    public void Part1()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(InputHelpers.ReadInputFile());
+
+        // Act
+        var actual = runner.RunPart1(input);
+
+        // Assert
+        actual.Should().Be(0);
+    }
+
+    [Fact]
+    public void Example_Part2()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(InputHelpers.ReadInputFile("example.txt"));
+
+        // Act
+        var actual = runner.RunPart2(input);
+
+        // Assert
+        actual.Should().Be(0);
+    }
+
+    [Fact]
+    public void Part2()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(InputHelpers.ReadInputFile());
+
+        // Act
+        var actual = runner.RunPart2(input);
+
+        // Assert
+        actual.Should().Be(0);
+    }
+
+    static IAoCRunner<string, int> CreateRunner() => new AoC.Y2022.Day10.Day10();
+}
