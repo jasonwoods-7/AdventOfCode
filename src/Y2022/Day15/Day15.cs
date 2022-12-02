4a81c6614d81591131a40bf0c573e661e6882feb
Jason Woods <jwoods@appliedsystems.com>
2022-12-01 21:56:42 -0600

    chore: adding AoC skeleton files

---
+++ src/Y2022/Day15/Day15.cs
@@ -0,0 +1,10 @@
+namespace AoC.Y2022.Day15;
+
+public class Day15 : IAoCRunner<string, int>
+{
+    public string ParseInput(string[] puzzleInput) => throw new NotImplementedException();
+
+    public int RunPart1(string input) => throw new NotImplementedException();
+
+    public int RunPart2(string input) => throw new NotImplementedException();
+}
