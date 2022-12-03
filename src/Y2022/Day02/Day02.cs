a8152044874c0f431c02d55804dfd89810358cf2
Jason Woods <jwoods@appliedsystems.com>
2022-12-02 21:08:03 -0600

    refactor: update day 2 to use constants

--- src/Y2022/Day02/Day02.cs
+++ src/Y2022/Day02/Day02.cs
@@ -2,35 +2,43 @@ namespace AoC.Y2022.Day02;
 
 public class Day02 : IAoCRunner<string[], int>
 {
+    const int RockPoints = 1;
+    const int PaperPoints = 2;
+    const int ScissorsPoints = 3;
+
+    const int LosePoints = 0;
+    const int TiePoints = 3;
+    const int WinPoints = 6;
+
     public string[] ParseInput(string[] puzzleInput) => puzzleInput;
 
     public int RunPart1(string[] input) => input
         .Sum(t => t switch
         {
-            "A X" => 1 + 3, // rock ties with rock
-            "B X" => 1 + 0, // rock loses to paper
-            "C X" => 1 + 6, // rock beats scissors
-            "A Y" => 2 + 6, // paper beats rock
-            "B Y" => 2 + 3, // paper ties with paper
-            "C Y" => 2 + 0, // paper loses to scissors
-            "A Z" => 3 + 0, // scissors loses to scissors
-            "B Z" => 3 + 6, // scissors beats paper
-            "C Z" => 3 + 3, // scissors ties with scissors
-            _ => 0
+            "A X" => RockPoints     + TiePoints,    // rock ties with rock
+            "B X" => RockPoints     + LosePoints,   // rock loses to paper
+            "C X" => RockPoints     + WinPoints,    // rock beats scissors
+            "A Y" => PaperPoints    + WinPoints,    // paper beats rock
+            "B Y" => PaperPoints    + TiePoints,    // paper ties with paper
+            "C Y" => PaperPoints    + LosePoints,   // paper loses to scissors
+            "A Z" => ScissorsPoints + LosePoints,   // scissors loses to scissors
+            "B Z" => ScissorsPoints + WinPoints,    // scissors beats paper
+            "C Z" => ScissorsPoints + TiePoints,    // scissors ties with scissors
+            _ => throw new InvalidOperationException("Invalid input")
         });
 
     public int RunPart2(string[] input) => input
         .Sum(t => t switch
         {
-            "A X" => 3 + 0, // rock beats scissors
-            "B X" => 1 + 0, // paper beats rock
-            "C X" => 2 + 0, // scissors beats paper
-            "A Y" => 1 + 3, // rock ties with rock
-            "B Y" => 2 + 3, // paper ties with paper
-            "C Y" => 3 + 3, // scissors ties with scissors
-            "A Z" => 2 + 6, // rock loses to paper
-            "B Z" => 3 + 6, // paper loses to scissors
-            "C Z" => 1 + 6, // scissors loses to rock
-            _ => 0
+            "A X" => LosePoints + ScissorsPoints,   // rock beats scissors
+            "B X" => LosePoints + RockPoints,       // paper beats rock
+            "C X" => LosePoints + PaperPoints,      // scissors beats paper
+            "A Y" => TiePoints  + RockPoints,       // rock ties with rock
+            "B Y" => TiePoints  + PaperPoints,      // paper ties with paper
+            "C Y" => TiePoints  + ScissorsPoints,   // scissors ties with scissors
+            "A Z" => WinPoints  + PaperPoints,      // rock loses to paper
+            "B Z" => WinPoints  + ScissorsPoints,   // paper loses to scissors
+            "C Z" => WinPoints  + RockPoints,       // scissors loses to rock
+            _ => throw new InvalidOperationException("Invalid input")
         });
 }
