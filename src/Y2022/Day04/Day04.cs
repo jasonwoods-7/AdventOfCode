5e256d8beb20311fe8fa47d4d56d343511dd8f78
Jason Woods <jwoods@appliedsystems.com>
2022-12-04 12:47:22 -0600

    refactor: seperate parsing logic and puzzle logic

--- src/Y2022/Day04/Day04.cs
+++ src/Y2022/Day04/Day04.cs
@@ -1,32 +1,38 @@
 namespace AoC.Y2022.Day04;
 
-public class Day04 : IAoCRunner<(int[] elf1, int[] elf2, int[] overlap)[], int>
+public class Day04 : IAoCRunner<(int, int, int, int)[], int>
 {
-    public (int[], int[], int[])[] ParseInput(string[] puzzleInput) => puzzleInput
+    public (int, int, int, int)[] ParseInput(string[] puzzleInput) => puzzleInput
         .Select(static l =>
         {
             var match = Regex.Match(l, @"(\d+)-(\d+),(\d+)-(\d+)");
             Debug.Assert(match.Success);
 
-            var elf1Begin = int.Parse(match.Groups[1].Value);
-            var elf1End = int.Parse(match.Groups[2].Value);
-            var elf1 = Enumerable.Range(elf1Begin, elf1End - elf1Begin + 1).ToArray();
-
-            var elf2Begin = int.Parse(match.Groups[3].Value);
-            var elf2End = int.Parse(match.Groups[4].Value);
-            var elf2 = Enumerable.Range(elf2Begin, elf2End - elf2Begin + 1).ToArray();
-
-            var overlap = elf1.Intersect(elf2).ToArray();
-
-            return (elf1, elf2, overlap);
+            return (
+                int.Parse(match.Groups[1].Value),
+                int.Parse(match.Groups[2].Value),
+                int.Parse(match.Groups[3].Value),
+                int.Parse(match.Groups[4].Value)
+            );
         })
         .ToArray();
 
-    public int RunPart1((int[] elf1, int[] elf2, int[] overlap)[] input) => input
+    public int RunPart1((int, int, int, int)[] input) => input
+        .Select(GetRangesAndOverlap)
         .Count(static t =>
-            t.overlap.Length == t.elf1.Length ||
-            t.overlap.Length == t.elf2.Length);
+            t.overlap.Length == t.elf1Range.Length ||
+            t.overlap.Length == t.elf2Range.Length);
 
-    public int RunPart2((int[] elf1, int[] elf2, int[] overlap)[] input) => input
+    public int RunPart2((int, int, int, int)[] input) => input
+        .Select(GetRangesAndOverlap)
         .Count(static t => t.overlap.Length != 0);
+
+    static (int[] elf1Range, int[] elf2Range, int[] overlap) GetRangesAndOverlap((int a, int b, int x, int y) input)
+    {
+        var elf1 = IntegerRange.FromMinMax(input.a, input.b, 1).ToArray();
+        var elf2 = IntegerRange.FromMinMax(input.x, input.y, 1).ToArray();
+        var overlap = elf1.Intersect(elf2).ToArray();
+
+        return (elf1, elf2, overlap);
+    }
 }
