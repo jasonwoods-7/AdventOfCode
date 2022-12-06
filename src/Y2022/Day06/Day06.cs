fa2bedec6244695d67e63a64994dd5361e194e6b
Jason Woods <jwoods@appliedsystems.com>
2022-12-05 23:29:18 -0600

    feat: adding 2022, day 6, parts 1 & 2

--- src/Y2022/Day06/Day06.cs
+++ src/Y2022/Day06/Day06.cs
@@ -1,10 +1,20 @@
+using static MoreLinq.Extensions.ChooseExtension;
+using static MoreLinq.Extensions.IndexExtension;
+using static MoreLinq.Extensions.WindowExtension;
+
 namespace AoC.Y2022.Day06;
 
-public class Day06 : IAoCRunner<IEnumerable<string>, int>
+public class Day06 : IAoCRunner<string, int>
 {
-    public IEnumerable<string> ParseInput(IEnumerable<string> puzzleInput) => puzzleInput;
+    public string ParseInput(IEnumerable<string> puzzleInput) => puzzleInput.First();
 
-    public int RunPart1(IEnumerable<string> input) => throw new NotImplementedException();
+    public int RunPart1(string input) => FindStartOfMessageMarker(input, 4);
 
-    public int RunPart2(IEnumerable<string> input) => throw new NotImplementedException();
+    public int RunPart2(string input) => FindStartOfMessageMarker(input, 14);
+
+    static int FindStartOfMessageMarker(string input, int windowSize) => input
+        .Window(windowSize)
+        .Index()
+        .Choose(p => (p.Value.ToHashSet().Count == windowSize, p.Key + windowSize))
+        .First();
 }
