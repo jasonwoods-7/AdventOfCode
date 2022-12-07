2977d3597f2e0620f2a2148a74d51ddeb0b27853
Jason Woods <jwoods@appliedsystems.com>
2022-12-06 23:58:50 -0600

    feat: adding 2022, day 7, parts 1 & 2

--- src/Y2022/Day07/Day07.cs
+++ src/Y2022/Day07/Day07.cs
@@ -1,10 +1,63 @@
 namespace AoC.Y2022.Day07;
 
-public class Day07 : IAoCRunner<IEnumerable<string>, int>
+public class Day07 : IAoCRunner<Directory, int>
 {
-    public IEnumerable<string> ParseInput(IEnumerable<string> puzzleInput) => puzzleInput;
+    public Directory ParseInput(IEnumerable<string> puzzleInput)
+    {
+        var root = new Directory("/", null);
+        var currentDirectory = root;
 
-    public int RunPart1(IEnumerable<string> input) => throw new NotImplementedException();
+        foreach (var command in puzzleInput.Skip(1))
+        {
+            if (command == "$ cd ..")
+            {
+                currentDirectory = currentDirectory.Parent!;
+            }
+            else if (command.StartsWith("$ cd "))
+            {
+                var newDirectory = command[5..];
+                currentDirectory = currentDirectory.Subdirectories.First(d => d.Name == newDirectory);
+            }
+            else if (command == "$ ls")
+            {
+            }
+            else if (command.StartsWith("dir "))
+            {
+                var name = command[4..];
+                currentDirectory.Subdirectories.Add(new Directory(name, currentDirectory));
+            }
+            else
+            {
+                var split = command.Split(" ");
+                currentDirectory.Files.Add((split[1], int.Parse(split[0])));
+            }
+        }
 
-    public int RunPart2(IEnumerable<string> input) => throw new NotImplementedException();
+        return root;
+    }
+
+    public int RunPart1(Directory input) => SubdirectorySizes(input)
+        .Where(s => s < 100_000)
+        .Sum();
+
+    public int RunPart2(Directory input)
+    {
+        var freeSpace = 70_000_000 - input.CalculateSize();
+        var requiredSpace = 30_000_000 - freeSpace;
+
+        return SubdirectorySizes(input)
+            .Where(s => s >= requiredSpace)
+            .Order()
+            .First();
+    }
+
+    static IEnumerable<int> SubdirectorySizes(Directory current)
+    {
+        yield return current.CalculateSize();
+
+        foreach (var sub in current.Subdirectories.SelectMany(SubdirectorySizes))
+        {
+            yield return sub;
+        }
+    }
 }
---
+++ src/Y2022/Day07/Directory.cs
@@ -0,0 +1,24 @@
+﻿namespace AoC.Y2022.Day07;
+
+public class Directory
+{
+    public Directory(string name, Directory? parent)
+    {
+        Name = name;
+        Parent = parent;
+        Subdirectories = new List<Directory>();
+        Files = new List<(string, int)>();
+    }
+
+    public string Name { get; }
+
+    public Directory? Parent { get; }
+
+    public List<Directory> Subdirectories { get; }
+
+    public List<(string name, int size)> Files { get; }
+
+    public int CalculateSize() =>
+        Files.Sum(f => f.size) +
+        Subdirectories.Sum(d => d.CalculateSize());
+}
