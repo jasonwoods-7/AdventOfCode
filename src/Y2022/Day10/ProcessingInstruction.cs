326f6e0b695db8885f5a72e09b1f4369fdd1c965
Jason Woods <jwoods@appliedsystems.com>
2022-12-10 16:00:17 -0600

    refactor: add processing instruction to simplify logic

---
+++ src/Y2022/Day10/ProcessingInstruction.cs
@@ -0,0 +1,13 @@
+﻿namespace AoC.Y2022.Day10;
+
+public class ProcessingInstruction : IInstruction
+{
+    public IInstruction ChildInstruction { get; }
+
+    public ProcessingInstruction(IInstruction childInstruction) =>
+        ChildInstruction = childInstruction;
+
+    public T Accept<T>(T visitor)
+        where T : IInstructionVisitor<T> =>
+        visitor.VisitProcessing(this);
+}
