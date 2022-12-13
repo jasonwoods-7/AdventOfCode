172240853befce29605e81e4dcb931378337d806
Jason Woods <jwoods@appliedsystems.com>
2022-12-12 22:51:27 -0600

    refactor: update number regex to find floats

--- src/Extensions/StringExtensions.cs
+++ src/Extensions/StringExtensions.cs
@@ -1,13 +1,15 @@
-﻿namespace AoC.Extensions;
+﻿using System.Numerics;
+
+namespace AoC.Extensions;
 
 public static partial class StringExtensions
 {
-    [GeneratedRegex(@"(\d+)")]
-    private static partial Regex IntegerRegex();
+    [GeneratedRegex(@"((?:\d+)(?:\.\d+)?)")]
+    private static partial Regex NumberRegex();
 
     public static IEnumerable<T> FindNumbers<T>(this string source)
-        where T : IParsable<T> =>
-        IntegerRegex()
+        where T : IParsable<T>, INumber<T> =>
+        NumberRegex()
             .Matches(source)
             .Select(m => T.Parse(m.Value, CultureInfo.CurrentCulture));
 }
