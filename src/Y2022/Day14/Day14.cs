120557fbc8ce3489fe88129ebb7a5725f0b5097d
Jason Woods <jwoods@appliedsystems.com>
2022-12-14 13:11:01 -0600

    feat: adding 2022, day 14, parts 1 & 2 (not optimized)

--- src/Y2022/Day14/Day14.cs
+++ src/Y2022/Day14/Day14.cs
@@ -1,10 +1,127 @@
 namespace AoC.Y2022.Day14;
 
-public class Day14 : IAoCRunner<IEnumerable<string>, int>
+public class Day14 : IAoCRunner<ImmutableHashSet<Coord>, int>
 {
-    public IEnumerable<string> ParseInput(IEnumerable<string> puzzleInput) => puzzleInput;
+    static readonly Coord InitialCoord = new(500, 0);
 
-    public int RunPart1(IEnumerable<string> input) => throw new NotImplementedException();
+    public ImmutableHashSet<Coord> ParseInput(IEnumerable<string> puzzleInput) => puzzleInput
+        .Select(static l => l
+            .Split(" -> ")
+            .Select(static p => p
+                .Split(",")
+                .Fold(static (x, y) => new Coord(
+                    int.Parse(x, CultureInfo.CurrentCulture),
+                    int.Parse(y, CultureInfo.CurrentCulture)))))
+        .SelectMany(static cs => cs
+            .Window(2)
+            .SelectMany(ws => ws
+                .Fold(static (w1, w2) =>
+                {
+                    Debug.Assert(w1.X == w2.X || w1.Y == w2.Y);
 
-    public int RunPart2(IEnumerable<string> input) => throw new NotImplementedException();
+                    var positions = new List<Coord>();
+
+                    if (w1.X == w2.X)
+                    {
+                        var minY = Math.Min(w1.Y, w2.Y);
+                        var maxY = Math.Max(w1.Y, w2.Y);
+
+                        for (var counter = minY; counter <= maxY; ++counter)
+                        {
+                            positions.Add(w1 with { Y = counter });
+                        }
+                    }
+                    else
+                    {
+                        var minX = Math.Min(w1.X, w2.X);
+                        var maxX = Math.Max(w1.X, w2.X);
+
+                        for (var counter = minX; counter <= maxX; ++counter)
+                        {
+                            positions.Add(w1 with { X = counter });
+                        }
+                    }
+
+                    return positions;
+                })))
+        .ToImmutableHashSet();
+
+    public int RunPart1(ImmutableHashSet<Coord> input)
+    {
+        var sandPos = ImmutableHashSet<Coord>.Empty;
+
+        // ReSharper disable ConvertToLocalFunction
+        Func<Coord, ImmutableHashSet<Coord>, bool> isOccupied = static (c, a) => a.Contains(c);
+
+        var maxY = input.MaxBy(static c => c.Y).Y;
+        Func<Coord, ImmutableHashSet<Coord>, bool> exitCondition = (c, _) => c.Y > maxY;
+        // ReSharper restore ConvertToLocalFunction
+
+        while (FindRestingPosition(
+            new Coord(500, 0),
+            input.Union(sandPos),
+            isOccupied,
+            exitCondition) is { IsSome: true } restingPosition)
+        {
+            sandPos = sandPos.Add(restingPosition.IfNone(default(Coord)));
+        }
+
+        return sandPos.Count;
+    }
+
+    public int RunPart2(ImmutableHashSet<Coord> input)
+    {
+        var sandPos = ImmutableHashSet<Coord>.Empty;
+
+        // ReSharper disable ConvertToLocalFunction
+        var maxY = input.MaxBy(static c => c.Y).Y;
+
+        Func<Coord, ImmutableHashSet<Coord>, bool> isOccupied = (c, a) => a.Contains(c) || c.Y == maxY + 2;
+
+        Func<Coord, ImmutableHashSet<Coord>, bool> exitCondition = static (c, a) => c == InitialCoord && a.Contains(c);
+        // ReSharper restore ConvertToLocalFunction
+
+        while (FindRestingPosition(
+            new Coord(500, 0),
+            input.Union(sandPos),
+            isOccupied,
+            exitCondition) is { IsSome: true } restingPosition)
+        {
+            sandPos = sandPos.Add(restingPosition.IfNone(default(Coord)));
+        }
+
+        return sandPos.Count;
+    }
+
+    static Option<Coord> FindRestingPosition(
+        Coord current,
+        ImmutableHashSet<Coord> currentlyOccupied,
+        Func<Coord, ImmutableHashSet<Coord>, bool> isOccupied,
+        Func<Coord, ImmutableHashSet<Coord>, bool> exitCondition)
+    {
+        if (exitCondition(current, currentlyOccupied))
+        {
+            return None;
+        }
+
+        var below = current.Below();
+        if (!isOccupied(below, currentlyOccupied))
+        {
+            return FindRestingPosition(below, currentlyOccupied, isOccupied, exitCondition);
+        }
+
+        var belowLeft = current.BelowLeft();
+        if (!isOccupied(belowLeft, currentlyOccupied))
+        {
+            return FindRestingPosition(belowLeft, currentlyOccupied, isOccupied, exitCondition);
+        }
+
+        var belowRight = current.BelowRight();
+        if (!isOccupied(belowRight, currentlyOccupied))
+        {
+            return FindRestingPosition(belowRight, currentlyOccupied, isOccupied, exitCondition);
+        }
+
+        return current;
+    }
 }
--- test/unit/Y2022/Day14/Day14Tests.cs
+++ test/unit/Y2022/Day14/Day14Tests.cs
@@ -1,9 +1,11 @@
+using AoC.Types;
+
 namespace AoC.Tests.Y2022.Day14;
 
 public class Day14Tests
 {
-    // [Fact]
-    public static void Example_Part1()
+    [Fact]
+    public void Example_Part1()
     {
         // Arrange
         var runner = CreateRunner();
@@ -14,11 +16,11 @@ public class Day14Tests
         var actual = runner.RunPart1(input);
 
         // Assert
-        actual.Should().Be(0);
+        actual.Should().Be(24);
     }
 
-    // [Fact]
-    public static void Part1()
+    [Fact]
+    public void Part1()
     {
         // Arrange
         var runner = CreateRunner();
@@ -29,11 +31,11 @@ public class Day14Tests
         var actual = runner.RunPart1(input);
 
         // Assert
-        actual.Should().Be(0);
+        actual.Should().Be(979);
     }
 
-    // [Fact]
-    public static void Example_Part2()
+    [Fact]
+    public void Example_Part2()
     {
         // Arrange
         var runner = CreateRunner();
@@ -44,11 +46,11 @@ public class Day14Tests
         var actual = runner.RunPart2(input);
 
         // Assert
-        actual.Should().Be(0);
+        actual.Should().Be(93);
     }
 
-    // [Fact]
-    public static void Part2()
+    [Fact(Skip = "Not optimized")]
+    public void Part2()
     {
         // Arrange
         var runner = CreateRunner();
@@ -59,8 +61,8 @@ public class Day14Tests
         var actual = runner.RunPart2(input);
 
         // Assert
-        actual.Should().Be(0);
+        actual.Should().Be(29_044);
     }
 
-    static IAoCRunner<IEnumerable<string>, int> CreateRunner() => new AoC.Y2022.Day14.Day14();
+    static IAoCRunner<ImmutableHashSet<Coord>, int> CreateRunner() => new AoC.Y2022.Day14.Day14();
 }
