62c93518226c9cbea753a4babd97937c20cac3f3
Jason Woods <jwoods@appliedsystems.com>
2022-12-17 16:30:35 -0600

    feat: adding 2022, day 17, parts 1 & 2

---
+++ src/Y2022/Day17/RepetitionChecker.cs
@@ -0,0 +1,31 @@
+using System.Text;
+
+namespace AoC.Y2022.Day17;
+
+partial class Day17
+{
+    readonly Dictionary<string, (long, long)> _cache = new();
+
+    Option<(long height, long rocks)> CheckForRepetition(IReadOnlySet<Coord> grid, long minY, long rock)
+    {
+        var normalizedRows = new StringBuilder(1000);
+
+        for (var y = 0; y < 100; ++y)
+        {
+            for (var x = 0; x < 7; ++x)
+            {
+                normalizedRows.Append(grid.Contains(new Coord(x, minY + y)) ? '#' : ' ');
+            }
+        }
+
+        var key = normalizedRows.ToString();
+        if (_cache.TryGetValue(key, out var value))
+        {
+            return value;
+        }
+
+        _cache.Add(key, (minY, rock));
+
+        return None;
+    }
+}
