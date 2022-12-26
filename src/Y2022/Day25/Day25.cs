ed517dba298a3a08faa3f4c19d772181b7d965bc
Jason Woods <jwoods@appliedsystems.com>
2022-12-26 14:40:47 -0600

    feat: adding 2022, day 25, parts 1 & 2

--- src/Y2022/Day25/Day25.cs
+++ src/Y2022/Day25/Day25.cs
@@ -1,10 +1,41 @@
 namespace AoC.Y2022.Day25;
 
-public class Day25 : IAoCRunner<IEnumerable<string>, int>
+public class Day25 : IAoCRunner<IEnumerable<long>, string>
 {
-    public IEnumerable<string> ParseInput(IEnumerable<string> puzzleInput) => puzzleInput;
+    public IEnumerable<long> ParseInput(IEnumerable<string> puzzleInput) => puzzleInput
+        .Select(FromSnafu);
 
-    public int RunPart1(IEnumerable<string> input) => throw new NotImplementedException();
+    public string RunPart1(IEnumerable<long> input) => input
+        .Sum()
+        .Apply(ToSnafu);
 
-    public int RunPart2(IEnumerable<string> input) => throw new NotImplementedException();
+    public string RunPart2(IEnumerable<long> input) => string.Empty;
+
+    static long FromSnafu(string snafu) => snafu
+        .Reverse()
+        .Index()
+        .Sum(t => (long)Math.Pow(5, t.index) * t.item switch
+        {
+            '=' => -2,
+            '-' => -1,
+            _ => t.item - '0'
+        });
+
+    static string ToSnafu(long number)
+    {
+        var snafuNum = "";
+
+        while (number > 0)
+        {
+            number = Math.DivRem(number, 5, out var remainder);
+            (snafuNum, number) = remainder switch
+            {
+                3 => ("=" + snafuNum, number + 1),
+                4 => ("-" + snafuNum, number + 1),
+                _ => (remainder.ToString(CultureInfo.CurrentCulture) + snafuNum, number)
+            };
+        }
+
+        return snafuNum;
+    }
 }
--- test/unit/Y2022/Day25/Day25Tests.cs
+++ test/unit/Y2022/Day25/Day25Tests.cs
@@ -2,8 +2,8 @@ namespace AoC.Tests.Y2022.Day25;
 
 public class Day25Tests
 {
-    // [Fact]
-    public static void Example_Part1()
+    [Fact]
+    public void Example_Part1()
     {
         // Arrange
         var runner = CreateRunner();
@@ -14,11 +14,11 @@ public class Day25Tests
         var actual = runner.RunPart1(input);
 
         // Assert
-        actual.Should().Be(0);
+        actual.Should().Be("2=-1=0");
     }
 
-    // [Fact]
-    public static void Part1()
+    [Fact]
+    public void Part1()
     {
         // Arrange
         var runner = CreateRunner();
@@ -29,11 +29,11 @@ public class Day25Tests
         var actual = runner.RunPart1(input);
 
         // Assert
-        actual.Should().Be(0);
+        actual.Should().Be("2=12-100--1012-0=012");
     }
 
-    // [Fact]
-    public static void Example_Part2()
+    [Fact]
+    public void Example_Part2()
     {
         // Arrange
         var runner = CreateRunner();
@@ -44,11 +44,11 @@ public class Day25Tests
         var actual = runner.RunPart2(input);
 
         // Assert
-        actual.Should().Be(0);
+        actual.Should().Be("");
     }
 
-    // [Fact]
-    public static void Part2()
+    [Fact]
+    public void Part2()
     {
         // Arrange
         var runner = CreateRunner();
@@ -59,8 +59,8 @@ public class Day25Tests
         var actual = runner.RunPart2(input);
 
         // Assert
-        actual.Should().Be(0);
+        actual.Should().Be("");
     }
 
-    static IAoCRunner<IEnumerable<string>, int> CreateRunner() => new AoC.Y2022.Day25.Day25();
+    static IAoCRunner<IEnumerable<long>, string> CreateRunner() => new AoC.Y2022.Day25.Day25();
 }
