d54f6c8a61cf0ba5c1e18af080033d3bb6898617
Jason Woods <jwoods@appliedsystems.com>
2023-12-01 23:44:36 -0600

    feat: adding 2023, day 2, parts 1 & 2

---
+++ test/unit/Y2023/Day02/Day02Tests.cs
@@ -0,0 +1,36 @@
+namespace AoC.Tests.Y2023.Day02;
+
+public class Day02Tests
+{
+    [Fact]
+    public void Part1()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(InputHelpers.ReadInputFile());
+
+        // Act
+        var result = runner.RunPart1(input);
+
+        // Assert
+        result.Should().Be(2_176);
+    }
+
+    [Fact]
+    public void Part2()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(InputHelpers.ReadInputFile());
+
+        // Act
+        var result = runner.RunPart2(input);
+
+        // Assert
+        result.Should().Be(63_700);
+    }
+
+    static IAoCRunner<IReadOnlyList<AoC.Y2023.Day02.Game>, int> CreateRunner() => new AoC.Y2023.Day02.Day02();
+}
