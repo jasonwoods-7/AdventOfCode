dad1e53c8134aa133cad9d3f9063d9661a3b3604
Jason Woods <jwoods@appliedsystems.com>
2023-12-04 20:16:32 -0600

    feat: adding 2023, day 4, parts 1 & 2

---
+++ src/Y2023/Day04/Day04.cs
@@ -0,0 +1,34 @@
+namespace AoC.Y2023.Day04;
+
+public class Day04 : IAoCRunner<IReadOnlyList<int>, int>
+{
+    public IReadOnlyList<int> ParseInput(IEnumerable<string> puzzleInput) => puzzleInput
+        .Select(static line => line.Split('|'))
+        .Select(static ss => ss[0]
+            .FindNumbers<int>()
+            .Skip(1)
+            .ToImmutableHashSet()
+            .Intersect(ss[1].FindNumbers<int>())
+            .Count)
+        .ToList();
+
+    public int RunPart1(IReadOnlyList<int> input) => input
+        .Sum(static t => (1 << t) >> 1);
+
+    public int RunPart2(IReadOnlyList<int> input)
+    {
+        var ticketCount = input
+            .Select(_ => 1)
+            .ToList();
+
+        for (var i = 0; i < input.Count; i++)
+        {
+            for (var j = 1; j <= input[i]; j++)
+            {
+                ticketCount[i + j] += ticketCount[i];
+            }
+        }
+
+        return ticketCount.Sum();
+    }
+}
---
+++ test/unit/Y2023/Day04/Day04Tests.cs
@@ -0,0 +1,66 @@
+namespace AoC.Tests.Y2023.Day04;
+
+public class Day04Tests
+{
+    [Fact]
+    public void Example_Part1()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(InputHelpers.ReadInputFile("example.txt"));
+
+        // Act
+        var result = runner.RunPart1(input);
+
+        // Assert
+        result.Should().Be(13);
+    }
+
+    [Fact]
+    public void Part1()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(InputHelpers.ReadInputFile());
+
+        // Act
+        var result = runner.RunPart1(input);
+
+        // Assert
+        result.Should().Be(18_519);
+    }
+
+    [Fact]
+    public void Example_Part2()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(InputHelpers.ReadInputFile("example.txt"));
+
+        // Act
+        var result = runner.RunPart2(input);
+
+        // Assert
+        result.Should().Be(30);
+    }
+
+    [Fact]
+    public void Part2()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(InputHelpers.ReadInputFile());
+
+        // Act
+        var result = runner.RunPart2(input);
+
+        // Assert
+        result.Should().Be(11_787_590);
+    }
+
+    static IAoCRunner<IReadOnlyList<int>, int> CreateRunner() => new AoC.Y2023.Day04.Day04();
+}
