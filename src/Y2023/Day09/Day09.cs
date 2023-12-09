efb70a995d306d267461c1f745c051ce302368d8
Jason Woods <jwoods@appliedsystems.com>
2023-12-09 00:36:24 -0600

    feat: adding 2023, day 9, parts 1 & 2

---
+++ src/Y2023/Day09/Day09.cs
@@ -0,0 +1,43 @@
+﻿namespace AoC.Y2023.Day09;
+
+public class Day09 : IAoCRunner<IReadOnlyList<IReadOnlyList<int>>, int>
+{
+    public IReadOnlyList<IReadOnlyList<int>> ParseInput(IEnumerable<string> puzzleInput) => puzzleInput
+        .Select(l => l.FindNumbers<int>().ToList())
+        .ToList();
+
+    public int RunPart1(IReadOnlyList<IReadOnlyList<int>> input) => input
+        .Sum(static e => Solve(ImmutableList.Create(e), FindNext));
+
+    public int RunPart2(IReadOnlyList<IReadOnlyList<int>> input) => input
+        .Sum(static e => Solve(ImmutableList.Create(e), FindPrevious));
+
+    static int Solve(
+        ImmutableList<IReadOnlyList<int>> current,
+        Func<ImmutableList<IReadOnlyList<int>>, int> finder)
+    {
+        var previous = current[^1];
+
+        if (previous.All(x => x == 0))
+        {
+            return finder(current);
+        }
+
+        var next = new List<int>();
+
+        for (var i = 0; i < previous.Count - 1; i++)
+        {
+            next.Add(previous[i + 1] - previous[i]);
+        }
+
+        return Solve(current.Add(next), finder);
+    }
+
+    static int FindNext(ImmutableList<IReadOnlyList<int>> current) => current
+        .Sum(c => c[^1]);
+
+    static int FindPrevious(ImmutableList<IReadOnlyList<int>> current) => current
+        .Select(c => c[0])
+        .Reverse()
+        .Aggregate((r, c) => c - r);
+}
---
+++ test/unit/Y2023/Day09/Day09Tests.cs
@@ -0,0 +1,66 @@
+﻿namespace AoC.Tests.Y2023.Day09;
+
+public class Day09Tests
+{
+    [Fact]
+    public void Example_Part1()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(InputHelpers.ReadInputFile("example.txt"));
+
+        // Act
+        var result = runner.RunPart1(input);
+
+        // Assert
+        result.Should().Be(114);
+    }
+
+    [SkippableFact]
+    public void Part1()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(InputHelpers.ReadInputFile());
+
+        // Act
+        var result = runner.RunPart1(input);
+
+        // Assert
+        result.Should().Be(1_789_635_132);
+    }
+
+    [Fact]
+    public void Example_Part2()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(InputHelpers.ReadInputFile("example.txt"));
+
+        // Act
+        var result = runner.RunPart2(input);
+
+        // Assert
+        result.Should().Be(2);
+    }
+
+    [SkippableFact]
+    public void Part2()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(InputHelpers.ReadInputFile());
+
+        // Act
+        var result = runner.RunPart2(input);
+
+        // Assert
+        result.Should().Be(913);
+    }
+
+    static IAoCRunner<IReadOnlyList<IReadOnlyList<int>>, int> CreateRunner() => new AoC.Y2023.Day09.Day09();
+}
