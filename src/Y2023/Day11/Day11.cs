afc65f0cfbdb8c1ea1ab24e33107d513868a3fb8
Jason Woods <jwoods@appliedsystems.com>
2023-12-11 16:02:11 -0600

    feat: adding 2023, day 11, parts 1 & 2

---
+++ src/Y2023/Day11/Day11.cs
@@ -0,0 +1,50 @@
+﻿namespace AoC.Y2023.Day11;
+
+public record Galaxy(IReadOnlyList<Coord> Coords)
+{
+    public Galaxy Expand(long multiplier)
+    {
+        var maxX = Coords.MaxBy(c => c.X).X;
+
+        var xEmpty = new List<long>();
+        var yEmpty = new List<long>();
+
+        for (var x = 0; x <= maxX; x++)
+        {
+            if (Coords.All(c => c.X != x))
+            {
+                xEmpty.Add(x);
+            }
+
+            if (Coords.All(c => c.Y != x))
+            {
+                yEmpty.Add(x);
+            }
+        }
+
+        return new Galaxy(Coords
+            .Select(c => new Coord(
+                c.X + (xEmpty.Count(x => c.X > x) * multiplier),
+                c.Y + (yEmpty.Count(y => c.Y > y) * multiplier)))
+            .ToList());
+    }
+}
+
+public class Day11(long multiplier) : IAoCRunner<Galaxy, long>
+{
+    public Galaxy ParseInput(IEnumerable<string> puzzleInput) => puzzleInput
+        .Index()
+        .SelectMany(y => y.item.Select((c, x) => (coord: new Coord(x, y.index), item: c)))
+        .Choose(t => (t.item == '#', t.coord))
+        .Apply(g => new Galaxy(g.ToList()));
+
+    public long RunPart1(Galaxy input) => Solve(input);
+
+    public long RunPart2(Galaxy input) => Solve(input);
+
+    long Solve(Galaxy input) => input
+        .Expand(multiplier)
+        .Coords
+        .Subsets(2)
+        .Sum(galaxies => galaxies.Fold((start, end) => start.ManhattanDistanceTo(end)));
+}
---
+++ test/unit/Y2023/Day11/Day11Tests.cs
@@ -0,0 +1,68 @@
+﻿namespace AoC.Tests.Y2023.Day11;
+
+public class Day11Tests
+{
+    [Fact]
+    public void Example_Part1()
+    {
+        // Arrange
+        var runner = CreateRunner(1L);
+
+        var input = runner.ParseInput(InputHelpers.ReadInputFile("example.txt"));
+
+        // Act
+        var result = runner.RunPart1(input);
+
+        // Assert
+        result.Should().Be(374L);
+    }
+
+    [SkippableFact]
+    public void Part1()
+    {
+        // Arrange
+        var runner = CreateRunner(1L);
+
+        var input = runner.ParseInput(InputHelpers.ReadInputFile());
+
+        // Act
+        var result = runner.RunPart1(input);
+
+        // Assert
+        result.Should().Be(9_947_476L);
+    }
+
+    [Theory]
+    [InlineData(10L, 1_030L)]
+    [InlineData(100L, 8_410L)]
+    public void Example_Part2(long multiplier, long expected)
+    {
+        // Arrange
+        var runner = CreateRunner(multiplier - 1L);
+
+        var input = runner.ParseInput(InputHelpers.ReadInputFile("example.txt"));
+
+        // Act
+        var result = runner.RunPart1(input);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [SkippableFact]
+    public void Part2()
+    {
+        // Arrange
+        var runner = CreateRunner(999_999L);
+
+        var input = runner.ParseInput(InputHelpers.ReadInputFile());
+
+        // Act
+        var result = runner.RunPart2(input);
+
+        // Assert
+        result.Should().Be(519_939_907_614L);
+    }
+
+    static IAoCRunner<AoC.Y2023.Day11.Galaxy, long> CreateRunner(long multiplier) => new AoC.Y2023.Day11.Day11(multiplier);
+}
