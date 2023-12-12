96438d29acc286638ec34f1baa4c146d932e8b05
Jason Woods <jwoods@appliedsystems.com>
2023-12-11 19:29:49 -0600

    refactor: day 11 - move galaxy into its own file

---
+++ src/Y2023/Day11/Galaxy.cs
@@ -0,0 +1,36 @@
+﻿namespace AoC.Y2023.Day11;
+
+public record Galaxy(IReadOnlyList<Coord> Coords)
+{
+    public long Solve(long multiplier) => Expand(multiplier)
+        .Coords
+        .Subsets(2)
+        .Sum(galaxies => galaxies.Fold((start, end) => start.ManhattanDistanceTo(end)));
+
+    Galaxy Expand(long multiplier)
+    {
+        var maxX = Coords.MaxBy(c => c.X).X;
+
+        var xEmpty = new List<long>();
+        var yEmpty = new List<long>();
+
+        for (var x = 0; x <= maxX; x++)
+        {
+            if (Coords.All(c => c.X != x))
+            {
+                xEmpty.Add(x);
+            }
+
+            if (Coords.All(c => c.Y != x))
+            {
+                yEmpty.Add(x);
+            }
+        }
+
+        return new Galaxy(Coords
+            .Select(c => new Coord(
+                c.X + (xEmpty.Count(x => c.X > x) * multiplier),
+                c.Y + (yEmpty.Count(y => c.Y > y) * multiplier)))
+            .ToList());
+    }
+}
