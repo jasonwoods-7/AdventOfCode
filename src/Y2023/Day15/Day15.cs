18ad65df12fd6f913230bea3a0fe5128cc40ec6b
Jason Woods <jwoods@appliedsystems.com>
2023-12-15 16:40:14 -0600

    feat: adding 2023, day 15, parts 1 & 2

---
+++ src/Y2023/Day15/Day15.cs
@@ -0,0 +1,63 @@
+﻿namespace AoC.Y2023.Day15;
+
+public partial class Day15 : IAoCRunner<IEnumerable<string>, int>
+{
+    [GeneratedRegex(@"(\w+)(?:(=\d)|(-))")]
+    private partial Regex OperationRegex();
+
+    public IEnumerable<string> ParseInput(IEnumerable<string> puzzleInput) => puzzleInput
+        .SelectMany(l => l.Split(','));
+
+    public int RunPart1(IEnumerable<string> input) => input
+        .Sum(Hash);
+
+    public int RunPart2(IEnumerable<string> input)
+    {
+        var boxes = new Dictionary<int, List<(string, int)>>();
+
+        foreach (var current in input)
+        {
+            var match = OperationRegex().Match(current);
+
+            var label = match.Groups[1].Value;
+            var hash = Hash(label);
+
+            if (match.Groups[2].Success)
+            {
+                AddLens(match.Groups[2].Value[1..].ParseNumber<int>(), boxes, hash, label);
+            }
+            else
+            {
+                if (boxes.TryGetValue(hash, out var value))
+                {
+                    value.RemoveAll(t => t.Item1 == label);
+                }
+            }
+        }
+
+        return boxes
+            .Sum(b => b.Value.Index(1).Sum(t => (b.Key + 1) * t.index * t.item.Item2));
+    }
+
+    static int Hash(string label) => label.Aggregate(0, (result, current) => ((result + current) * 17) & 0xFF);
+
+    static void AddLens(int lens, Dictionary<int, List<(string, int)>> boxes, int hash, string label)
+    {
+        if (boxes.TryGetValue(hash, out var value))
+        {
+            var existing = value.FindIndex(t => t.Item1 == label);
+            if (existing != -1)
+            {
+                value[existing] = (label, lens);
+            }
+            else
+            {
+                value.Add((label, lens));
+            }
+        }
+        else
+        {
+            boxes.Add(hash, [(label, lens)]);
+        }
+    }
+}
---
+++ test/unit/Y2023/Day15/Day15Tests.cs
@@ -0,0 +1,53 @@
+﻿using AoC.Tests.Extensions;
+
+namespace AoC.Tests.Y2023.Day15;
+
+public class Day15Tests
+{
+    [SkippableFact]
+    public void Part1()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(InputHelpers.ReadInputFile());
+
+        // Act
+        var result = runner.RunPart1(input);
+
+        // Assert
+        result.Should().Be(511_343);
+    }
+
+    [Fact]
+    public void Example_Part2()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput("rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7");
+
+        // Act
+        var result = runner.RunPart2(input);
+
+        // Assert
+        result.Should().Be(145);
+    }
+
+    [SkippableFact]
+    public void Part2()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(InputHelpers.ReadInputFile());
+
+        // Act
+        var result = runner.RunPart2(input);
+
+        // Assert
+        result.Should().Be(294_474);
+    }
+
+    static IAoCRunner<IEnumerable<string>, int> CreateRunner() => new AoC.Y2023.Day15.Day15();
+}
