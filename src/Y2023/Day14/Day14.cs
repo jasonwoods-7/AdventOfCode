40729c9ceb75dc6e45c7158ab4ad2abb35cd7f13
Jason Woods <jwoods@appliedsystems.com>
2023-12-14 20:56:21 -0600

    feat: adding 2023, day 14, parts 1 & 2

---
+++ src/Y2023/Day14/Day14.cs
@@ -0,0 +1,99 @@
+﻿using System.Text;
+using Dish = System.Collections.Generic.IReadOnlyDictionary<AoC.Types.Coord, char>;
+
+namespace AoC.Y2023.Day14;
+
+public class Day14(ILogger<Day14> logger) : IAoCRunner<Dish, long>
+{
+    public Dish ParseInput(IEnumerable<string> puzzleInput) => puzzleInput
+        .Index()
+        .SelectMany(y => y.item.Select((c, x) => (new Coord(x, y.index), c)))
+        .ToDictionary();
+
+    public long RunPart1(Dish input)
+    {
+        var tilt = Tilt(input, 0, 1, 0, 1, (h, v) => new Coord(h, v));
+
+        var maxY = input.Select(c => c.Key.Y).Max();
+
+        return Score(tilt, maxY);
+    }
+
+    public long RunPart2(Dish input)
+    {
+        var maxY = input.Select(c => c.Key.Y).Max();
+
+        var spun = MultiSpin(input, 1_000, maxY);
+
+        PrintMap(spun, maxY);
+
+        return Score(spun, maxY);
+    }
+
+    void PrintMap(Dish dish, long max)
+    {
+        for (var y = 0; y <= max; ++y)
+        {
+            var builder = new StringBuilder((int)max);
+
+            for (var x = 0; x <= max; ++x)
+            {
+                builder.Append(dish[new Coord(x, y)]);
+            }
+
+#pragma warning disable CA1848
+            logger.LogDebug("{Line}", builder.ToString());
+#pragma warning restore CA1848
+        }
+    }
+
+    static long Score(Dish dish, long max) => dish
+        .Where(v => v.Value == 'O')
+        .Sum(k => max - k.Key.Y + 1);
+
+    static Dish MultiSpin(Dish dish, long count, long max)
+    {
+        while (count-- > 0)
+        {
+            dish = Spin(dish, max);
+        }
+
+        return dish;
+    }
+
+    static Dish Spin(Dish dish, long max) => dish
+        .Apply(d => Tilt(d, 0, 1, 0, 1, (h, v) => new Coord(h, v)))
+        .Apply(d => Tilt(d, max, -1, 0, 1, (h, v) => new Coord(v, h)))
+        .Apply(d => Tilt(d, max, -1, max, -1, (h, v) => new Coord(h, v)))
+        .Apply(d => Tilt(d, 0, 1, max, -1, (h, v) => new Coord(v, h)))
+        ;
+
+    static Dish Tilt(Dish dish, long hStart, long hInc, long vStart, long vInc, Func<long, long, Coord> createCoord)
+    {
+        var nextDish = dish
+            .ToDictionary(k => k.Key, v => v.Value);
+
+        for (var horizontal = hStart; dish.ContainsKey(createCoord(horizontal, 0)); horizontal += hInc)
+        {
+            var lastV = vStart;
+
+            for (var vertical = vStart; dish.ContainsKey(createCoord(0, vertical)); vertical += vInc)
+            {
+                var current = dish[createCoord(horizontal, vertical)];
+
+                if (current == '#')
+                {
+                    lastV = vertical + vInc;
+                }
+                else if (current == 'O')
+                {
+                    nextDish[createCoord(horizontal, vertical)] = '.';
+                    nextDish[createCoord(horizontal, lastV)] = 'O';
+                    lastV += vInc;
+                }
+            }
+        }
+
+        return nextDish;
+    }
+}
---
+++ test/unit/Y2023/Day14/Day14Tests.cs
@@ -0,0 +1,69 @@
+﻿namespace AoC.Tests.Y2023.Day14;
+
+public class Day14Tests(ITestOutputHelper outputHelper)
+{
+    readonly ILoggerFactory _loggerFactory = outputHelper.CreateLoggerFactory();
+
+    [Fact]
+    public void Example_Part1()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(InputHelpers.ReadInputFile("example.txt"));
+
+        // Act
+        var result = runner.RunPart1(input);
+
+        // Assert
+        result.Should().Be(136);
+    }
+
+    [SkippableFact]
+    public void Part1()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(InputHelpers.ReadInputFile());
+
+        // Act
+        var result = runner.RunPart1(input);
+
+        // Assert
+        result.Should().Be(108_935L);
+    }
+
+    [Fact]
+    public void Example_Part2()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(InputHelpers.ReadInputFile("example.txt"));
+
+        // Act
+        var result = runner.RunPart2(input);
+
+        // Assert
+        result.Should().Be(64);
+    }
+
+    [SkippableFact]
+    public void Part2()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(InputHelpers.ReadInputFile());
+
+        // Act
+        var result = runner.RunPart2(input);
+
+        // Assert
+        result.Should().Be(100_876L);
+    }
+
+    IAoCRunner<IReadOnlyDictionary<Coord, char>, long> CreateRunner() => new AoC.Y2023.Day14.Day14(
+        _loggerFactory.CreateLogger<AoC.Y2023.Day14.Day14>());
+}
