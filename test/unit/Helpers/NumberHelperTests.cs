bdc7e44ddad067fdf4eaf9ec88cc2077f4c58d0b
Jason Woods <jwoods@appliedsystems.com>
2023-12-28 23:25:56 -0600

    test: test updates

--- test/unit/Helpers/NumberHelperTests.cs
+++ test/unit/Helpers/NumberHelperTests.cs
@@ -7,6 +7,7 @@ public class NumberHelperTests
     [Theory]
     [InlineData(4, 9, 1)]
     [InlineData(40, 6, 2)]
+    [InlineData(1, 37, 1)]
     public void GreatestCommonDivisor(long num1, long num2, long expected)
     {
         // Arrange
@@ -51,7 +52,6 @@ public class NumberHelperTests
     [InlineData(40, 1, 0)]
     [InlineData(52, -217, 96)]
     [InlineData(-486, 217, 121)]
-    [InlineData(40, 2018, -1)]
     public void ModInverse(long num, long mod, long expected)
     {
         // Arrange
@@ -65,6 +65,9 @@ public class NumberHelperTests
 
     [Theory]
     [InlineData(new long[] { 3, 5, 7 }, new long[] { 2, 3, 2 }, 23)]
+    [InlineData(new long[] { 5, 7, 9, 11 }, new long[] { 1, 2, 3, 4 }, 1731)]
+    [InlineData(new long[] { 11, 12, 13 }, new long[] { 10, 4, 12 }, 1000)]
+    [InlineData(new long[] { 100, 23 }, new long[] { 19, 0 }, 1219)]
     public void ChineseRemainderTheorem(long[] n, long[] a, long expected)
     {
         // Arrange
