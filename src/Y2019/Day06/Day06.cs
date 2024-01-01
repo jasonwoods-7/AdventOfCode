9b09e8d15b1a3cfd30d59ea88fe5ed592fb88f53
Jason Woods <jwoods@appliedsystems.com>
2024-01-01 00:27:35 -0600

    feat: adding 2019, day 6, parts 1 & 2

---
+++ src/Y2019/Day06/Day06.cs
@@ -0,0 +1,60 @@
+﻿namespace AoC.Y2019.Day06;
+
+public record Node(string Name)
+{
+    public Node? Parent { get; set; }
+    public IList<Node> Children { get; } = new List<Node>();
+
+    public long Height(long currentHeight = 0) => Parent
+        ?.Height(currentHeight + 1)
+        ?? currentHeight;
+}
+
+public class Day06 : IAoCRunner<IReadOnlyDictionary<string, Node>, long>
+{
+    public IReadOnlyDictionary<string, Node> ParseInput(IEnumerable<string> puzzleInput)
+    {
+        var orbits = puzzleInput
+            .Select(l => l.Split(')').Fold((parent, child) => (parent, child)))
+            .ToList();
+
+        var nodes = orbits
+            .SelectMany(o => new[] { o.parent, o.child })
+            .Distinct()
+            .Select(n => new Node(n))
+            .ToDictionary(n => n.Name);
+
+        foreach (var (p, c) in orbits)
+        {
+            nodes[p].Children.Add(nodes[c]);
+            nodes[c].Parent = nodes[p];
+        }
+
+        return nodes;
+    }
+
+    public long RunPart1(IReadOnlyDictionary<string, Node> input) => input.Sum(n => n.Value.Height());
+
+    public long RunPart2(IReadOnlyDictionary<string, Node> input)
+    {
+        return SuperEnumerable
+            .GetShortestPathCost<Node, long>(
+                input["YOU"],
+                GetChildren,
+                input["SAN"])
+            - 2;
+
+        static IEnumerable<(Node, long)> GetChildren(Node current, long cost)
+        {
+            if (current.Parent is not null)
+            {
+                yield return (current.Parent, cost + 1);
+            }
+
+            foreach (var child in current.Children)
+            {
+                yield return (child, cost + 1);
+            }
+        }
+    }
+}
---
+++ test/unit/Y2019/Day06/Day06Tests.cs
@@ -0,0 +1,64 @@
+﻿namespace AoC.Tests.Y2019.Day06;
+
+[SuppressMessage("ReSharper", "AsyncApostle.AsyncMethodNamingHighlighting")]
+[SuppressMessage("ReSharper", "AsyncApostle.ConfigureAwaitHighlighting")]
+public class Day06Tests : AoCRunnerTests<AoC.Y2019.Day06.Day06>
+{
+    [Fact]
+    public async Task Example_Part1()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync("example1.txt"));
+
+        // Act
+        var result = runner.RunPart1(input);
+
+        // Assert
+        result.Should().Be(42L);
+    }
+
+    public override async Task Part1()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync());
+
+        // Act
+        var result = runner.RunPart1(input);
+
+        // Assert
+        result.Should().Be(312_697L);
+    }
+
+    [Fact]
+    public async Task Example_Part2()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync("example2.txt"));
+
+        // Act
+        var result = runner.RunPart2(input);
+
+        // Assert
+        result.Should().Be(4L);
+    }
+
+    public override async Task Part2()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync());
+
+        // Act
+        var result = runner.RunPart2(input);
+
+        // Assert
+        result.Should().Be(466L);
+    }
+}
