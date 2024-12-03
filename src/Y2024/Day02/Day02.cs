97cc4fcc5195fe11d3fc5b73fe177e33b9044470
Jason Woods <jwoods@appliedsystems.com>
2024-12-02 20:48:13 -0600

    refactor: 2024, day 2, shared logic

--- src/Y2024/Day02/Day02.cs
+++ src/Y2024/Day02/Day02.cs
@@ -10,19 +10,7 @@ public class Day02 : IAoCRunner<IReadOnlyList<Report>, int>
             .ToList();
 
     public int RunPart1(IReadOnlyList<Report> input) => input
-        .Count(r => r
-            .Levels
-            .Zip(r.Levels.Skip(1), (x, y) => (Math.Abs(x - y) is 1 or 2 or 3, x < y))
-            .Aggregate((true, 0), (res, t) =>
-            {
-                return res switch
-                {
-                    (true, -1) => (t is { Item1: true, Item2: false }, -1),
-                    (true, 0) => (t.Item1, t.Item2 ? 1 : -1),
-                    (true, 1) => (t is { Item1: true, Item2: true }, 1),
-                    _ => res
-                };
-            }, res => res.Item1));
+        .Count(r => IsReportSafe(r.Levels));
 
     public int RunPart2(IReadOnlyList<Report> input) => input
         .Count(r => r
@@ -30,16 +18,18 @@ public class Day02 : IAoCRunner<IReadOnlyList<Report>, int>
             .Subsets(r.Levels.Count - 1)
             .Cast<IReadOnlyList<int>>()
             .Append(r.Levels)
-            .Any(s => s
-                .Zip(s.Skip(1), (x, y) => (Math.Abs(x - y) is 1 or 2 or 3, x < y))
-                .Aggregate((true, 0), (res, t) =>
-                {
-                    return res switch
-                    {
-                        (true, -1) => (t is { Item1: true, Item2: false }, -1),
-                        (true, 0) => (t.Item1, t.Item2 ? 1 : -1),
-                        (true, 1) => (t is { Item1: true, Item2: true }, 1),
-                        _ => res
-                    };
-                }, res => res.Item1)));
+            .Any(IsReportSafe));
+
+    static bool IsReportSafe(IReadOnlyList<int> levels) => levels
+        .Zip(levels.Skip(1), (x, y) => (Math.Abs(x - y) is 1 or 2 or 3, x < y))
+        .Aggregate((true, 0), (res, t) =>
+        {
+            return res switch
+            {
+                (true, -1) => (t is { Item1: true, Item2: false }, -1),
+                (true, 0) => (t.Item1, t.Item2 ? 1 : -1),
+                (true, 1) => (t is { Item1: true, Item2: true }, 1),
+                _ => res
+            };
+        }, res => res.Item1);
 }
