c487a81161364db546c153409582c946b36a6214
Jason Woods <jwoods@appliedsystems.com>
2024-12-02 23:42:05 -0600

    feat: add 2024, day 3, parts 1 & 2

---
+++ src/Y2024/Day03/Day03.cs
@@ -0,0 +1,35 @@
+﻿namespace AoC.Y2024.Day03;
+
+public partial class Day03 : IAoCRunner<IReadOnlyList<string>, int>
+{
+    [GeneratedRegex(@"mul\((\d{1,3}),(\d{1,3})\)")]
+    private partial Regex Part1Instructions();
+
+    [GeneratedRegex(@"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)")]
+    private partial Regex Part2Instructions();
+
+    public IReadOnlyList<string> ParseInput(IEnumerable<string> puzzleInput) => puzzleInput.ToList();
+
+    public int RunPart1(IReadOnlyList<string> input) => input
+        .SelectMany(
+            s => Part1Instructions().Matches(s),
+            (_, m) => MultiplyValues(m))
+        .Sum();
+
+    public int RunPart2(IReadOnlyList<string> input) => input
+        .SelectMany(s => Part2Instructions().Matches(s))
+        .Aggregate(
+            (true, 0),
+            (result, current) => current.Value switch
+            {
+                "do()" => (true, result.Item2),
+                "don't()" => (false, result.Item2),
+                _ when result.Item1 => (true, result.Item2 + MultiplyValues(current)),
+                _ => result
+            },
+            result => result.Item2);
+
+    static int MultiplyValues(Match match) =>
+        match.Groups[1].Value.ParseNumber<int>() *
+        match.Groups[2].Value.ParseNumber<int>();
+}
---
+++ test/unit/Y2024/Day03/Day03Tests.cs
@@ -0,0 +1,34 @@
+﻿namespace AoC.Tests.Y2024.Day03;
+
+[SuppressMessage("ReSharper", "AsyncApostle.ConfigureAwaitHighlighting")]
+[SuppressMessage("ReSharper", "AsyncApostle.AsyncMethodNamingHighlighting")]
+public class Day03Tests : AoCRunnerTests<AoC.Y2024.Day03.Day03>
+{
+    public override async Task Part1()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync());
+
+        // Act
+        var result = runner.RunPart1(input);
+
+        // Assert
+        result.Should().Be(174_960_292);
+    }
+
+    public override async Task Part2()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync());
+
+        // Act
+        var result = runner.RunPart2(input);
+
+        // Assert
+        result.Should().Be(56_275_602);
+    }
+}
