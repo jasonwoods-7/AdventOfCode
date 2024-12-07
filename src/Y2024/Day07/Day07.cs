02c107e42f8a7e483f27513f869ec133ddde4ec8
Jason Woods <jwoods@appliedsystems.com>
2024-12-07 00:00:49 -0600

    feat: add 2024, day 7, parts 1 & 2

---
+++ src/Y2024/Day07/Day07.cs
@@ -0,0 +1,51 @@
+﻿namespace AoC.Y2024.Day07;
+
+public record ParsedInput(IReadOnlyList<Equation> Equations);
+public record Equation(long Result, IReadOnlyList<long> Values);
+
+public class Day07 : IAoCRunner<ParsedInput, long>
+{
+    public ParsedInput ParseInput(IEnumerable<string> puzzleInput) => puzzleInput
+        .Select(l => l.FindNumbers<long>().ToList() switch
+        {
+            [var head, .. var rest] => new Equation(head, rest),
+            _ => throw new InvalidOperationException()
+        })
+        .Apply(eqs => new ParsedInput(eqs.ToList()));
+
+    public long RunPart1(ParsedInput input) => input
+        .Equations
+        .Choose(eq =>
+        {
+            var head = eq.Values.Head();
+            var tail = eq.Values.Tail();
+
+            var values = tail
+                .Aggregate(
+                    new List<long> { head },
+                    (res, cur) => res
+                        .SelectMany(c => new[] { c * cur, c + cur })
+                        .ToList());
+
+            return (values.Contains(eq.Result), eq.Result);
+        })
+        .Sum();
+
+    public long RunPart2(ParsedInput input) => input
+        .Equations
+        .Choose(eq =>
+        {
+            var head = eq.Values.Head();
+            var tail = eq.Values.Tail();
+
+            var values = tail
+                .Aggregate(
+                    new List<long> { head },
+                    (res, cur) => res
+                        .SelectMany(c => new[] { c * cur, c + cur, $"{c}{cur}".ParseNumber<long>() })
+                        .ToList());
+
+            return (values.Contains(eq.Result), eq.Result);
+        })
+        .Sum();
+}
---
+++ test/unit/Y2024/Day07/Day07Tests.cs
@@ -0,0 +1,64 @@
+﻿namespace AoC.Tests.Y2024.Day07;
+
+[SuppressMessage("ReSharper", "AsyncApostle.ConfigureAwaitHighlighting")]
+[SuppressMessage("ReSharper", "AsyncApostle.AsyncMethodNamingHighlighting")]
+public class Day07Tests : AoCRunnerTests<AoC.Y2024.Day07.Day07>
+{
+    [Fact]
+    public async Task Part1Example()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync("example.txt"));
+
+        // Act
+        var result = runner.RunPart1(input);
+
+        // Assert
+        result.Should().Be(3_749);
+    }
+
+    public override async Task Part1()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync());
+
+        // Act
+        var result = runner.RunPart1(input);
+
+        // Assert
+        result.Should().Be(1_260_333_054_159L);
+    }
+
+    [Fact]
+    public async Task Part2Example()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync("example.txt"));
+
+        // Act
+        var result = runner.RunPart2(input);
+
+        // Assert
+        result.Should().Be(11_387);
+    }
+
+    public override async Task Part2()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync());
+
+        // Act
+        var result = runner.RunPart2(input);
+
+        // Assert
+        result.Should().Be(162_042_343_638_683L);
+    }
+}
