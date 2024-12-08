a27dc2c606209399245fb66e8b47778955b562a8
Jason Woods <jwoods@appliedsystems.com>
2024-12-08 00:04:07 -0600

    feat: add 2024, day 8, parts 1 & 2

---
+++ src/Y2024/Day08/Day08.cs
@@ -0,0 +1,64 @@
+﻿using ParsedInput = System.Collections.Generic.IReadOnlyDictionary<char, System.Collections.Generic.IReadOnlyList<AoC.Types.Coord>>;
+
+namespace AoC.Y2024.Day08;
+
+public class Day08 : IAoCRunner<ParsedInput, int>
+{
+    public ParsedInput ParseInput(IEnumerable<string> puzzleInput) => puzzleInput
+        .Index()
+        .SelectMany(l => l.Item.Index(), (l, c) => (x: c.Index, y: l.Index, c: c.Item))
+        .GroupBy(t => t.c, t => new Coord(t.x, t.y))
+        .ToDictionary(g => g.Key, IReadOnlyList<Coord> (g) => g.ToList());
+
+    public int RunPart1(ParsedInput input)
+    {
+        var bounds = Bounds.FindBounds(input.Values.SelectMany(c => c));
+
+        return input
+            .Choose(kvp => (kvp.Key != '.', kvp.Value))
+            .SelectMany(
+                v => v.Subsets(2),
+                (_, s) => s.Fold((x, y) =>
+                {
+                    var slope = x - y;
+                    return new[] { x + slope, y - slope };
+                }))
+            .SelectMany(c => c)
+            .ToHashSet()
+            .Count(c => bounds.WithinBounds(c));
+    }
+
+    public int RunPart2(ParsedInput input)
+    {
+        var bounds = Bounds.FindBounds(input.Values.SelectMany(c => c));
+
+        return input
+            .Choose(kvp => (kvp.Key != '.', kvp.Value))
+            .SelectMany(
+                v => v.Subsets(2),
+                (_, s) => s.Fold((x, y) =>
+                {
+                    var slope = x - y;
+                    var result = new List<Coord> { x, y };
+
+                    var next = x + slope;
+                    while (bounds.WithinBounds(next))
+                    {
+                        result.Add(next);
+                        next += slope;
+                    }
+
+                    next = y - slope;
+                    while (bounds.WithinBounds(next))
+                    {
+                        result.Add(next);
+                        next -= slope;
+                    }
+
+                    return result;
+                }))
+            .SelectMany(c => c)
+            .ToHashSet()
+            .Count;
+    }
+}
---
+++ test/unit/Y2024/Day08/Day08Tests.cs
@@ -0,0 +1,64 @@
+﻿namespace AoC.Tests.Y2024.Day08;
+
+[SuppressMessage("ReSharper", "AsyncApostle.ConfigureAwaitHighlighting")]
+[SuppressMessage("ReSharper", "AsyncApostle.AsyncMethodNamingHighlighting")]
+public class Day08Tests : AoCRunnerTests<AoC.Y2024.Day08.Day08>
+{
+    [Fact]
+    public async Task Part1Example()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync("example.txt"));
+
+        // Act
+        var result = runner.RunPart1(input);
+
+        // Assert
+        result.Should().Be(14);
+    }
+
+    public override async Task Part1()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync());
+
+        // Act
+        var result = runner.RunPart1(input);
+
+        // Assert
+        result.Should().Be(396);
+    }
+
+    [Fact]
+    public async Task Part2Example()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync("example.txt"));
+
+        // Act
+        var result = runner.RunPart2(input);
+
+        // Assert
+        result.Should().Be(34);
+    }
+
+    public override async Task Part2()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync());
+
+        // Act
+        var result = runner.RunPart2(input);
+
+        // Assert
+        result.Should().Be(1_200);
+    }
+}
