9bddf32c31084683e3dd1f9ec8fbac8773a539e9
Jason Woods <jwoods@appliedsystems.com>
2024-12-09 15:06:58 -0600

    feat: add 2024, day 9, parts 1 & 2

---
+++ src/Y2024/Day09/Day09.cs
@@ -0,0 +1,127 @@
+﻿using ParsedInput = System.Collections.Generic.IReadOnlyList<int>;
+
+namespace AoC.Y2024.Day09;
+
+public class Day09 : IAoCRunner<ParsedInput, long>
+{
+    public ParsedInput ParseInput(IEnumerable<string> puzzleInput) => puzzleInput
+        .SelectMany(l => l, (_, c) => c - '0')
+        .ToList();
+
+    public long RunPart1(ParsedInput input)
+    {
+        var memory = input
+            .Index()
+            .SelectMany(t => Enumerable.Repeat((t.Index & 1) == 0 ? t.Index >> 1 : -1, t.Item))
+            .ToList();
+
+        var end = memory.Count - 1;
+        var start = 0;
+
+        while (end > start)
+        {
+            while (memory[end] == -1)
+            {
+                end--;
+            }
+
+            var current = memory[end];
+            memory[end] = -1;
+
+            while (memory[start] != -1)
+            {
+                start++;
+            }
+
+            memory[start] = current;
+        }
+
+        var sum = 0L;
+
+        for (var index = 0; memory[index] != -1; ++index)
+        {
+            sum += index * memory[index];
+        }
+
+        return sum;
+    }
+
+    public long RunPart2(ParsedInput input)
+    {
+        var memory = input
+            .Index()
+            .SelectMany(t => Enumerable.Repeat((t.Index & 1) == 0 ? t.Index >> 1 : -1, t.Item))
+            .ToList();
+
+        var end = memory.Count - 1;
+        var start = 0;
+
+        while (end > start)
+        {
+            var savedStart = memory.IndexOf(-1);
+
+            while (memory[end] == -1)
+            {
+                end--;
+            }
+
+            var file = memory[end];
+            var fileEnd = end--;
+            while (memory[end] == file)
+            {
+                end--;
+            }
+
+            var fileStart = end + 1;
+            var fileLength = fileEnd - fileStart;
+
+            var moved = false;
+
+            while (end > start && !moved)
+            {
+                while (memory[start] != -1)
+                {
+                    start++;
+                }
+
+                var freeStart = start;
+                while (start < memory.Count && memory[start] == -1)
+                {
+                    start++;
+                }
+
+                var freeEnd = --start;
+                var freeLength = freeEnd - freeStart;
+
+                if (freeLength >= fileLength && fileStart > freeStart)
+                {
+                    for (var counter = 0; counter <= fileLength; ++counter)
+                    {
+                        memory[freeStart + counter] = file;
+                        memory[fileStart + counter] = -1;
+                    }
+
+                    moved = true;
+                }
+                else
+                {
+                    start = freeEnd + 1;
+                }
+            }
+
+            start = savedStart;
+        }
+
+        var sum = 0L;
+
+        for (var index = 0; index < memory.Count; ++index)
+        {
+            if (memory[index] != -1)
+            {
+                sum += index * memory[index];
+            }
+        }
+
+        return sum;
+    }
+}
---
+++ test/unit/Y2024/Day09/Day09Tests.cs
@@ -0,0 +1,64 @@
+﻿namespace AoC.Tests.Y2024.Day09;
+
+[SuppressMessage("ReSharper", "AsyncApostle.ConfigureAwaitHighlighting")]
+[SuppressMessage("ReSharper", "AsyncApostle.AsyncMethodNamingHighlighting")]
+public class Day09Tests : AoCRunnerTests<AoC.Y2024.Day09.Day09>
+{
+    [Fact]
+    public void Part1Example()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(["2333133121414131402"]);
+
+        // Act
+        var result = runner.RunPart1(input);
+
+        // Assert
+        result.Should().Be(1_928);
+    }
+
+    public override async Task Part1()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync());
+
+        // Act
+        var result = runner.RunPart1(input);
+
+        // Assert
+        result.Should().Be(6_353_658_451_014L);
+    }
+
+    [Fact]
+    public void Part2Example()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(["2333133121414131402"]);
+
+        // Act
+        var result = runner.RunPart2(input);
+
+        // Assert
+        result.Should().Be(2_858);
+    }
+
+    public override async Task Part2()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync());
+
+        // Act
+        var result = runner.RunPart2(input);
+
+        // Assert
+        result.Should().Be(6_382_582_136_592L);
+    }
+}
