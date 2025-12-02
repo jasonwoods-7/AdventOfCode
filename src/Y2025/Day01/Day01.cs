dedeb25d7fcccb61100fb95f2af6f80977ab90a9
Jason Woods <jwoods@appliedsystems.com>
2025-12-01 18:27:17 -0600

    feat: add 2025, day 1, parts 1 & 2

---
+++ src/Y2025/Day01/Day01.cs
@@ -0,0 +1,83 @@
+﻿using System.Runtime.CompilerServices;
+using ParsedInput = System.Collections.Immutable.ImmutableList<(char, int)>;
+
+namespace AoC.Y2025.Day01;
+
+public class Day01(ILogger<Day01> logger) : IAoCRunner<ParsedInput, int>
+{
+    public ParsedInput ParseInput(IEnumerable<string> puzzleInput) =>
+        puzzleInput.Select(p => (p[0], p[1..].ParseNumber<int>())).ToImmutableList();
+
+    public int RunPart1(ParsedInput input) =>
+        input.Aggregate(
+            (current: 50, count: 0),
+            (t, c) =>
+            {
+                var next =
+                    (
+                        t.current
+                        + c switch
+                        {
+                            ('L', var n) => -n,
+                            ('R', var n) => n,
+                            _ => throw new SwitchExpressionException(),
+                        }
+                    ) % 100;
+
+                while (next < 0)
+                {
+                    next += 100;
+                }
+
+                return (next, t.count + (next == 0 ? 1 : 0));
+            },
+            t => t.count
+        );
+
+    public int RunPart2(ParsedInput input) =>
+        input.Aggregate(
+            (current: 50, count: 0),
+            (t, c) =>
+            {
+                var zeroes = 0;
+
+                var next =
+                    t.current
+                    + c switch
+                    {
+                        ('L', var n) => -n,
+                        ('R', var n) => n,
+                        _ => throw new SwitchExpressionException(),
+                    };
+
+                var first = true;
+                while (next < 0)
+                {
+                    if (t.current == 0 && first)
+                    {
+                        zeroes--;
+                        first = false;
+                    }
+
+                    zeroes++;
+                    next += 100;
+                }
+
+                if (next == 0)
+                {
+                    zeroes++;
+                }
+
+                while (next >= 100)
+                {
+                    zeroes++;
+                    next -= 100;
+                }
+
+                logger.LogDebug("Total zeroes: {Zeroes}", t.count + zeroes);
+
+                return (next, t.count + zeroes);
+            },
+            t => t.count
+        );
+}
---
+++ test/unit/Y2025/Day01/Day01Tests.cs
@@ -0,0 +1,119 @@
+﻿using AoC.Tests.Extensions;
+
+namespace AoC.Tests.Y2025.Day01;
+
+[SuppressMessage("ReSharper", "AsyncApostle.AsyncMethodNamingHighlighting")]
+[SuppressMessage("ReSharper", "AsyncApostle.ConfigureAwaitHighlighting")]
+public class Day01Tests(ITestOutputHelper outputHelper) : AoCRunnerTests<AoC.Y2025.Day01.Day01>
+{
+    [Fact]
+    public async Task Part1Example()
+    {
+        // Arrange
+        using var loggerFactory = outputHelper.CreateLoggerFactory();
+        var logger = loggerFactory.CreateLogger<AoC.Y2025.Day01.Day01>();
+
+        var runner = CreateRunner(logger);
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync("example.txt"));
+
+        // Act
+        var result = runner.RunPart1(input);
+
+        // Assert
+        result.Should().Be(3);
+    }
+
+    public override async Task Part1()
+    {
+        // Arrange
+        using var loggerFactory = outputHelper.CreateLoggerFactory();
+        var logger = loggerFactory.CreateLogger<AoC.Y2025.Day01.Day01>();
+
+        var runner = CreateRunner(logger);
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync());
+
+        // Act
+        var result = runner.RunPart1(input);
+
+        // Assert
+        result.Should().Be(1_152);
+    }
+
+    [Fact]
+    public async Task Part2Example()
+    {
+        // Arrange
+        using var loggerFactory = outputHelper.CreateLoggerFactory();
+        var logger = loggerFactory.CreateLogger<AoC.Y2025.Day01.Day01>();
+
+        var runner = CreateRunner(logger);
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync("example.txt"));
+
+        // Act
+        var result = runner.RunPart2(input);
+
+        // Assert
+        result.Should().Be(6);
+    }
+
+    [Theory]
+    [InlineData("L150", 2)]
+    [InlineData("R150", 2)]
+    [InlineData(
+        """
+            L50
+            R101
+            """,
+        2
+    )]
+    [InlineData(
+        """
+            R50
+            R50
+            L50
+            L50
+            R75
+            L50
+            L25
+            L75
+            R50
+            """,
+        6
+    )]
+    public void Part2EdgeCases(string rawInput, int expected)
+    {
+        // Arrange
+        using var loggerFactory = outputHelper.CreateLoggerFactory();
+        var logger = loggerFactory.CreateLogger<AoC.Y2025.Day01.Day01>();
+
+        var runner = CreateRunner(logger);
+
+        var input = runner.ParseInput(rawInput);
+
+        // Act
+        var result = runner.RunPart2(input);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    public override async Task Part2()
+    {
+        // Arrange
+        using var loggerFactory = outputHelper.CreateLoggerFactory();
+        var logger = loggerFactory.CreateLogger<AoC.Y2025.Day01.Day01>();
+
+        var runner = CreateRunner(logger);
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync());
+
+        // Act
+        var result = runner.RunPart2(input);
+
+        // Assert
+        result.Should().Be(6_671);
+    }
+}
