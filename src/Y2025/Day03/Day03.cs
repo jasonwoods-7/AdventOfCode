f7a84770e92468169c6f996096a8504874eed448
Jason Woods <jwoods@appliedsystems.com>
2025-12-03 00:28:52 -0600

    feat: add 2025, day 3, parts 1 & 2

---
+++ src/Y2025/Day03/Day03.cs
@@ -0,0 +1,41 @@
+﻿using ParsedInput = System.Collections.Immutable.ImmutableList<System.ReadOnlyMemory<long>>;
+
+namespace AoC.Y2025.Day03;
+
+public class Day03 : IAoCRunner<ParsedInput, long>
+{
+    public ParsedInput ParseInput(IEnumerable<string> puzzleInput) =>
+        puzzleInput
+            .Select(l => new ReadOnlyMemory<long>(l.Select(c => (long)c - '0').ToArray()))
+            .ToImmutableList();
+
+    public long RunPart1(ParsedInput input) => input.Sum(v => SolveLine(v, 2));
+
+    public long RunPart2(ParsedInput input) => input.Sum(v => SolveLine(v, 12));
+
+    static long SolveLine(ReadOnlyMemory<long> line, int remaining, long accumulator = 0)
+    {
+        if (remaining == 0)
+        {
+            return accumulator;
+        }
+
+        var currentSlice = line[..^(remaining - 1)];
+
+        var max = -1L;
+        var maxIndex = -1;
+
+        for (var counter = 0; counter < currentSlice.Length; ++counter)
+        {
+            var current = currentSlice.Span[counter];
+
+            if (current > max)
+            {
+                max = current;
+                maxIndex = counter;
+            }
+        }
+
+        return SolveLine(line[(maxIndex + 1)..], remaining - 1, (accumulator * 10) + max);
+    }
+}
---
+++ test/unit/Y2025/Day03/Day03Tests.cs
@@ -0,0 +1,64 @@
+﻿namespace AoC.Tests.Y2025.Day03;
+
+[SuppressMessage("ReSharper", "AsyncApostle.AsyncMethodNamingHighlighting")]
+[SuppressMessage("ReSharper", "AsyncApostle.ConfigureAwaitHighlighting")]
+public class Day03Tests : AoCRunnerTests<AoC.Y2025.Day03.Day03>
+{
+    [Fact]
+    public async Task Part1Example()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync("example.txt"));
+
+        // Act
+        var result = runner.RunPart1(input);
+
+        // Assert
+        result.Should().Be(357);
+    }
+
+    public override async Task Part1()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync());
+
+        // Act
+        var result = runner.RunPart1(input);
+
+        // Assert
+        result.Should().Be(16_927);
+    }
+
+    [Fact]
+    public async Task Part2Example()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync("example.txt"));
+
+        // Act
+        var result = runner.RunPart2(input);
+
+        // Assert
+        result.Should().Be(3_121_910_778_619L);
+    }
+
+    public override async Task Part2()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync());
+
+        // Act
+        var result = runner.RunPart2(input);
+
+        // Assert
+        result.Should().Be(167_384_358_365_132L);
+    }
+}
