b15552a40ee629bae71e7c67a2e695302bd684dc
Jason Woods <jwoods@appliedsystems.com>
2025-12-02 19:18:55 -0600

    feat: add 2025, day 2, parts 1 & 2

---
+++ src/Y2025/Day02/Day02.cs
@@ -0,0 +1,80 @@
+﻿using ParsedInput = System.Collections.Immutable.ImmutableList<(long lo, long hi)>;
+
+namespace AoC.Y2025.Day02;
+
+public class Day02 : IAoCRunner<ParsedInput, long>
+{
+    public ParsedInput ParseInput(IEnumerable<string> puzzleInput) =>
+        puzzleInput
+            .Single()
+            .Split(',')
+            .Select(r => r.FindNumbers<long>().Fold((l, h) => (l, -h)))
+            .ToImmutableList();
+
+    public long RunPart1(ParsedInput input) => Runner(input, Part1IsInvalid);
+
+    public long RunPart2(ParsedInput input) => Runner(input, Part2IsInvalid);
+
+    static long Runner(ParsedInput input, Func<long, bool> predicate) =>
+        input
+            .SelectMany(
+                r => EnumerableExtensions.Range(r.lo, r.hi + 1, l => l + 1),
+                (_, r) => predicate(r) ? r : 0
+            )
+            .Sum();
+
+    static bool Part1IsInvalid(long id)
+    {
+        var s = id.ToString(CultureInfo.InvariantCulture);
+
+        if ((s.Length & 1) == 1)
+        {
+            return false;
+        }
+
+        var half = s.Length >> 1;
+
+        for (var counter = 0; counter < half; ++counter)
+        {
+            if (s[counter] != s[counter + half])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static bool Part2IsInvalid(long id)
+    {
+        if (id < 10)
+        {
+            return false;
+        }
+
+        var s = id.ToString(CultureInfo.InvariantCulture);
+
+        var half = (s.Length + 1) >> 1;
+
+        for (var outer = 1; outer <= half; ++outer)
+        {
+            var pattern = s[..outer];
+
+            var potentialMatch = s.Length % outer == 0;
+
+            for (var inner = outer; potentialMatch && inner < s.Length; inner += outer)
+            {
+                var candidate = s[inner..(inner + outer)];
+
+                potentialMatch = string.Equals(pattern, candidate, StringComparison.Ordinal);
+            }
+
+            if (potentialMatch)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ test/unit/Y2025/Day02/Day02Tests.cs
@@ -0,0 +1,64 @@
+﻿namespace AoC.Tests.Y2025.Day02;
+
+[SuppressMessage("ReSharper", "AsyncApostle.AsyncMethodNamingHighlighting")]
+[SuppressMessage("ReSharper", "AsyncApostle.ConfigureAwaitHighlighting")]
+public class Day02Tests : AoCRunnerTests<AoC.Y2025.Day02.Day02>
+{
+    [Fact]
+    public async Task Part1Example()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync("example.txt"));
+
+        // Act
+        var result = runner.RunPart1(input);
+
+        // Assert
+        result.Should().Be(1_227_775_554);
+    }
+
+    public override async Task Part1()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync());
+
+        // Act
+        var result = runner.RunPart1(input);
+
+        // Assert
+        result.Should().Be(30_323_879_646L);
+    }
+
+    [Fact]
+    public async Task Part2Example()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync("example.txt"));
+
+        // Act
+        var result = runner.RunPart2(input);
+
+        // Assert
+        result.Should().Be(4_174_379_265);
+    }
+
+    public override async Task Part2()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync());
+
+        // Act
+        var result = runner.RunPart2(input);
+
+        // Assert
+        result.Should().Be(43_872_163_557L);
+    }
+}
