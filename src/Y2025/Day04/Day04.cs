75745d1a62393efbe7ff9851f11119ee852112e8
Jason Woods <jwoods@appliedsystems.com>
2025-12-04 19:37:36 -0600

    feat: add 2025, day 4, parts 1 & 2

---
+++ src/Y2025/Day04/Day04.cs
@@ -0,0 +1,39 @@
+﻿using ParsedInput = System.Collections.Immutable.ImmutableHashSet<AoC.Types.Coord>;
+
+namespace AoC.Y2025.Day04;
+
+public class Day04 : IAoCRunner<ParsedInput, long>
+{
+    public ParsedInput ParseInput(IEnumerable<string> puzzleInput) =>
+        puzzleInput
+            .SelectMany(
+                (line, y) => line.Cartesian([y]).Index(),
+                (_, t) => (t.Item.Item1 == '@', x: t.Index, y: t.Item.Item2)
+            )
+            .Choose(t => (t.Item1, new Coord(t.x, t.y)))
+            .ToImmutableHashSet();
+
+    public long RunPart1(ParsedInput input) =>
+        input.Count(c => c.Adjacent().Count(input.Contains) < 4);
+
+    public long RunPart2(ParsedInput input)
+    {
+        var currentCount = input.Count;
+        int previousCount;
+        var removed = 0;
+
+        do
+        {
+            previousCount = currentCount;
+
+            var toRemove = input.Where(c => c.Adjacent().Count(input.Contains) < 4).ToList();
+
+            input = input.Except(toRemove);
+
+            currentCount = input.Count;
+            removed += toRemove.Count;
+        } while (previousCount != currentCount);
+
+        return removed;
+    }
+}
---
+++ test/unit/Y2025/Day04/Day04Tests.cs
@@ -0,0 +1,64 @@
+﻿namespace AoC.Tests.Y2025.Day04;
+
+[SuppressMessage("ReSharper", "AsyncApostle.AsyncMethodNamingHighlighting")]
+[SuppressMessage("ReSharper", "AsyncApostle.ConfigureAwaitHighlighting")]
+public class Day04Tests : AoCRunnerTests<AoC.Y2025.Day04.Day04>
+{
+    [Fact]
+    public async Task Part1Example()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync("example.txt"));
+
+        // Act
+        var result = runner.RunPart1(input);
+
+        // Assert
+        result.Should().Be(13);
+    }
+
+    public override async Task Part1()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync());
+
+        // Act
+        var result = runner.RunPart1(input);
+
+        // Assert
+        result.Should().Be(1_508);
+    }
+
+    [Fact]
+    public async Task Part2Example()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync("example.txt"));
+
+        // Act
+        var result = runner.RunPart2(input);
+
+        // Assert
+        result.Should().Be(43);
+    }
+
+    public override async Task Part2()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync());
+
+        // Act
+        var result = runner.RunPart2(input);
+
+        // Assert
+        result.Should().Be(8_538);
+    }
+}
