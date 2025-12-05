fb74981e80c32f99632dc496dc938ec3aea7dbe0
Jason Woods <jwoods@appliedsystems.com>
2025-12-05 17:11:26 -0600

    feat: add 2025, day 5, parts 1 & 2

---
+++ src/Y2025/Day05/Day05.cs
@@ -0,0 +1,71 @@
+﻿namespace AoC.Y2025.Day05;
+
+public record ParsedInput(
+    ImmutableList<(long lo, long hi)> Ranges,
+    ImmutableList<long> Ingredients
+);
+
+public class Day05 : IAoCRunner<ParsedInput, long>
+{
+    public ParsedInput ParseInput(IEnumerable<string> puzzleInput) =>
+        puzzleInput
+            .Split(l => l.Length == 0)
+            .Fold(
+                (ranges, ingredients) =>
+                    new ParsedInput(
+                        ranges
+                            .Select(r => r.FindNumbers<long>().Fold((lo, hi) => (lo, -hi)))
+                            .ToImmutableList(),
+                        ingredients.Select(i => i.ParseNumber<long>()).ToImmutableList()
+                    )
+            );
+
+    public long RunPart1(ParsedInput input) =>
+        input.Ingredients.Count(i => input.Ranges.Any(r => i >= r.lo && i <= r.hi));
+
+    public long RunPart2(ParsedInput input) =>
+        input
+            .Ranges.OrderBy(r => r.lo)
+            .ThenBy(r => r.hi)
+            .Aggregate(
+                ImmutableList<(long lo, long hi)>.Empty,
+                (accumulator, current) =>
+                {
+                    for (var counter = 0; counter < accumulator.Count; ++counter)
+                    {
+                        if (
+                            accumulator[counter].lo <= current.lo
+                            && accumulator[counter].hi >= current.hi
+                        )
+                        {
+                            return accumulator;
+                        }
+
+                        if (
+                            accumulator[counter].hi >= current.lo
+                            && current.hi >= accumulator[counter].hi
+                        )
+                        {
+                            return accumulator.SetItem(
+                                counter,
+                                (accumulator[counter].lo, current.hi)
+                            );
+                        }
+
+                        if (
+                            accumulator[counter].lo <= current.hi
+                            && current.lo <= accumulator[counter].lo
+                        )
+                        {
+                            return accumulator.SetItem(
+                                counter,
+                                (current.lo, accumulator[counter].hi)
+                            );
+                        }
+                    }
+
+                    return accumulator.Add(current);
+                },
+                accumulator => accumulator.Sum(r => r.hi - r.lo + 1)
+            );
+}
---
+++ test/unit/Y2025/Day05/Day05Tests.cs
@@ -0,0 +1,64 @@
+﻿namespace AoC.Tests.Y2025.Day05;
+
+[SuppressMessage("ReSharper", "AsyncApostle.AsyncMethodNamingHighlighting")]
+[SuppressMessage("ReSharper", "AsyncApostle.ConfigureAwaitHighlighting")]
+public class Day05Tests : AoCRunnerTests<AoC.Y2025.Day05.Day05>
+{
+    [Fact]
+    public async Task Part1Example()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync("example.txt"));
+
+        // Act
+        var result = runner.RunPart1(input);
+
+        // Assert
+        result.Should().Be(3);
+    }
+
+    public override async Task Part1()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync());
+
+        // Act
+        var result = runner.RunPart1(input);
+
+        // Assert
+        result.Should().Be(865);
+    }
+
+    [Fact]
+    public async Task Part2Example()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync("example.txt"));
+
+        // Act
+        var result = runner.RunPart2(input);
+
+        // Assert
+        result.Should().Be(14);
+    }
+
+    public override async Task Part2()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync());
+
+        // Act
+        var result = runner.RunPart2(input);
+
+        // Assert
+        result.Should().Be(352_556_672_963_116L);
+    }
+}
