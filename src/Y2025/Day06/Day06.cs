9f4bae908456b067da2ff432dbc43afbd9380eb7
Jason Woods <jwoods@appliedsystems.com>
2025-12-06 14:12:20 -0600

    feat: add 2025, day 6, parts 1 & 2

---
+++ src/Y2025/Day06/Day06.cs
@@ -0,0 +1,121 @@
+﻿using CommunityToolkit.HighPerformance;
+
+namespace AoC.Y2025.Day06;
+
+public record ParsedInput(ReadOnlyMemory2D<char> Digits, ImmutableList<char> Operators);
+
+public class Day06 : IAoCRunner<ParsedInput, long>
+{
+    public ParsedInput ParseInput(IEnumerable<string> puzzleInput) =>
+        puzzleInput.Partition(
+            l => !l.ContainsAny(['+', '*']),
+            (digits, operators) =>
+            {
+                var digs = digits.ToArray();
+                var ops = operators.Single().Where(o => o != ' ').ToImmutableList();
+
+                return new ParsedInput(
+                    new ReadOnlyMemory2D<char>(
+                        digs.SelectMany(l => l).ToArray(),
+                        digs.Length,
+                        digs[0].Length
+                    ),
+                    ops
+                );
+            }
+        );
+
+    public long RunPart1(ParsedInput input)
+    {
+        return Solver(input, NumberParser);
+
+        static long[,] NumberParser(ReadOnlyMemory2D<char> digits, int opCount)
+        {
+            var result = new long[opCount, digits.Height];
+
+            for (var row = 0; row < digits.Height; ++row)
+            {
+                var nums = new string(digits.Span.GetRowSpan(row)).FindNumbers<long>().ToImmutableList();
+
+                for (var col = 0; col < nums.Count; ++col)
+                {
+                    result[col, row] = nums[col];
+                }
+            }
+
+            return result;
+        }
+    }
+
+    public long RunPart2(ParsedInput input)
+    {
+        return Solver(input, NumberParser);
+
+        static long[,] NumberParser(ReadOnlyMemory2D<char> digits, int opCount)
+        {
+            var result = new long[opCount, digits.Height];
+
+            var current = ImmutableList.CreateBuilder<long>();
+            var row = 0;
+
+            for (var column = 0; column < digits.Width; ++column)
+            {
+                var value = new string(digits.Span.GetColumn(column).ToArray()).Trim();
+
+                if (value.Length == 0)
+                {
+                    for (var counter = 0; counter < current.Count; ++counter)
+                    {
+                        result[row, counter] = current[counter];
+                    }
+
+                    current.Clear();
+                    row++;
+                }
+                else
+                {
+                    current.Add(value.ParseNumber<long>());
+                }
+            }
+
+            for (var counter = 0; counter < current.Count; ++counter)
+            {
+                result[row, counter] = current[counter];
+            }
+
+            return result;
+        }
+    }
+
+    static long Solver(ParsedInput input, Func<ReadOnlyMemory2D<char>, int, long[,]> numberParser)
+    {
+        var result = 0L;
+
+        var opCount = input.Operators.Count;
+
+        var numbers = numberParser(input.Digits, opCount);
+
+        for (var y = 0; y < opCount; ++y)
+        {
+            var aggregator = GetAggregator(input.Operators[y]);
+            var value = numbers[y, 0];
+
+            for (var x = 1; x < numbers.GetLength(1); ++x)
+            {
+                var num = numbers[y, x];
+
+                if (num != 0)
+                {
+                    value = aggregator(value, num);
+                }
+            }
+
+            result += value;
+        }
+
+        return result;
+
+        static Func<long, long, long> GetAggregator(char op) =>
+            op == '+' ? (a, b) => a + b : (a, b) => a * b;
+    }
+}
---
+++ test/unit/Y2025/Day06/Day06Tests.cs
@@ -0,0 +1,64 @@
+﻿namespace AoC.Tests.Y2025.Day06;
+
+[SuppressMessage("ReSharper", "AsyncApostle.AsyncMethodNamingHighlighting")]
+[SuppressMessage("ReSharper", "AsyncApostle.ConfigureAwaitHighlighting")]
+public class Day06Tests : AoCRunnerTests<AoC.Y2025.Day06.Day06>
+{
+    [Fact]
+    public async Task Part1Example()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync("example.txt"));
+
+        // Act
+        var result = runner.RunPart1(input);
+
+        // Assert
+        result.ShouldBe(4_277_556);
+    }
+
+    public override async Task Part1()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync());
+
+        // Act
+        var result = runner.RunPart1(input);
+
+        // Assert
+        result.ShouldBe(5_346_286_649_122L);
+    }
+
+    [Fact]
+    public async Task Part2Example()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync("example.txt"));
+
+        // Act
+        var result = runner.RunPart2(input);
+
+        // Assert
+        result.ShouldBe(3_263_827);
+    }
+
+    public override async Task Part2()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync());
+
+        // Act
+        var result = runner.RunPart2(input);
+
+        // Assert
+        result.ShouldBe(10_389_131_401_929L);
+    }
+}
