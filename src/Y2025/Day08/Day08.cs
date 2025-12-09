4073bbb94a8a8f335cd0cd5764d2c0dc4db35d82
Jason Woods <jwoods@appliedsystems.com>
2025-12-08 20:07:53 -0600

    feat: add 2025, day 8, parts 1 & 2

---
+++ src/Y2025/Day08/Day08.cs
@@ -0,0 +1,94 @@
+﻿using ParsedInput = System.Collections.Immutable.ImmutableList<AoC.Types.Coord3d>;
+
+namespace AoC.Y2025.Day08;
+
+public class Day08 : IAoCRunner<ParsedInput, long>
+{
+    public ParsedInput ParseInput(IEnumerable<string> puzzleInput) =>
+        puzzleInput
+            .Select(l => l.FindNumbers<int>().Fold((x, y, z) => new Coord3d(x, y, z)))
+            .ToImmutableList();
+
+    public long RunPart1(
+        ParsedInput input,
+        object[]? additionalParams = null,
+        CancellationToken cancellationToken = default
+    )
+    {
+        var (boxes, _) = Solver(input, (int)additionalParams![0]);
+
+        return boxes.Select(b => b.Count).OrderDescending().Take(3).Product();
+    }
+
+    public long RunPart2(
+        ParsedInput input,
+        object[]? additionalParams = null,
+        CancellationToken cancellationToken = default
+    )
+    {
+        var (_, lastAdded) = Solver(input, -1);
+
+        return lastAdded.Item1.X * lastAdded.Item2.X;
+    }
+
+    static (ImmutableList<ImmutableHashSet<Coord3d>> boxes, (Coord3d, Coord3d) lastAdded) Solver(
+        ParsedInput input,
+        int remainingConnections
+    ) =>
+        input
+            .Subsets(2)
+            .OrderBy(c => c.Fold((f, s) => f.DistanceTo(s)))
+            .Aggregate(
+                (
+                    boxes: ImmutableList<ImmutableHashSet<Coord3d>>.Empty,
+                    lastAdded: (Coord3d.Zero, Coord3d.Zero),
+                    remainingConnections
+                ),
+                (accumulator, current) =>
+                {
+                    if (accumulator.remainingConnections == 0)
+                    {
+                        return accumulator;
+                    }
+
+                    var indexes = accumulator
+                        .boxes.Index()
+                        .Choose(t =>
+                            (t.Item.Contains(current[0]) || t.Item.Contains(current[1]), t.Index)
+                        )
+                        .ToList();
+
+                    if (indexes.Count == 0)
+                    {
+                        return (
+                            accumulator.boxes.Add(
+                                ImmutableHashSet<Coord3d>.Empty.AddRange([current[0], current[1]])
+                            ),
+                            (current[0], current[1]),
+                            accumulator.remainingConnections - 1
+                        );
+                    }
+
+                    var next = accumulator.boxes;
+                    var junction = accumulator.boxes[indexes[0]].AddRange([current[0], current[1]]);
+
+                    var lastAdded =
+                        junction == accumulator.boxes[indexes[0]]
+                            ? accumulator.lastAdded
+                            : (current[0], current[1]);
+
+                    for (var counter = 1; counter < indexes.Count; ++counter)
+                    {
+                        junction = junction.AddRange(accumulator.boxes[indexes[counter]]);
+                        next = accumulator.boxes.RemoveAt(indexes[counter] - counter + 1);
+                    }
+
+                    return (
+                        next.SetItem(indexes[0], junction),
+                        lastAdded,
+                        accumulator.remainingConnections - 1
+                    );
+                },
+                accumulator => (accumulator.boxes, accumulator.lastAdded)
+            );
+}
---
+++ test/unit/Y2025/Day08/Day08Tests.cs
@@ -0,0 +1,64 @@
+﻿namespace AoC.Tests.Y2025.Day08;
+
+[SuppressMessage("ReSharper", "AsyncApostle.AsyncMethodNamingHighlighting")]
+[SuppressMessage("ReSharper", "AsyncApostle.ConfigureAwaitHighlighting")]
+public class Day08Tests : AoCRunnerTests<AoC.Y2025.Day08.Day08>
+{
+    [Fact]
+    public async Task Part1Example()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync("example.txt"));
+
+        // Act
+        var result = runner.RunPart1(input, [10]);
+
+        // Assert
+        result.ShouldBe(40);
+    }
+
+    public override async Task Part1()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync());
+
+        // Act
+        var result = runner.RunPart1(input, [1_000]);
+
+        // Assert
+        result.ShouldBe(75_680);
+    }
+
+    [Fact]
+    public async Task Part2Example()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync("example.txt"));
+
+        // Act
+        var result = runner.RunPart2(input, [-1]);
+
+        // Assert
+        result.ShouldBe(25_272);
+    }
+
+    public override async Task Part2()
+    {
+        // Arrange
+        var runner = CreateRunner();
+
+        var input = runner.ParseInput(await InputHelpers.ReadInputFileAsync());
+
+        // Act
+        var result = runner.RunPart2(input);
+
+        // Assert
+        result.ShouldBe(8_995_844_880L);
+    }
+}
